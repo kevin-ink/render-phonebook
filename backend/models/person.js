02c96d2422386d3f698de1c3cca6d9a2d8e1f02e
Kevin <kevinsl192k@gmail.com>
2026-03-18 13:07:30 -0700

    added validation to person schema

--- backend/models/person.js
+++ backend/models/person.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose')
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+    },
+    required: true,
+    minlength: 8,
+  },
 })
 
 personSchema.set('toJSON', {
